5132151f367c38c04d42eba1474ab3728546f88c
Randy Lust <rclust@gmail.com>
2026-08-09 13:03:59 -0400

    feat: add private continuity and memory dashboard

--- custom_components/home_context_memory/const.py
+++ custom_components/home_context_memory/const.py
@@ -1,6 +1,10 @@
 DOMAIN = "home_context_memory"
 PLATFORM = "conversation"
+PLATFORMS = ("conversation", "sensor", "button")
 DEFAULT_ROLLING_DAYS = 30
 DEFAULT_MAX_RESULTS = 3
 STORAGE_VERSION = 1
 STORAGE_KEY = DOMAIN
+
+SERVICE_FORGET = "forget"
+SERVICE_CLEAR = "clear"
